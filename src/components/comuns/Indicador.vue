f2fc7f335e141d2a7fb4d005f69cb5c58b7f8277
Botseeker <jorge-asf@hotmail.com>
2022-03-04 17:06:48 -0400

    compnt.son*indicador

---
+++ src/components/comuns/Indicador.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="'h-100 p-5 rouded-3 border ${bg} ${color}'">
+    <p>{{ titulo }}</p>
+    <h2>{{ indicador }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Indicador-',
+    props: ['titulo', 'indicador', 'bg', 'color']
+}
+</script>
+
+<style>
+</style>

022feb3e2a62f0c383bad2a57d41192e1402e844
Botseeker <jorge-asf@hotmail.com>
2022-03-11 16:45:44 -0400

    Add*pacote-mitt

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//importar o pacote mitt
+import mitt from 'mitt'
+//criar instancia do pacote mitt
+const emitter = mitt()
 
-createApp(App).mount('#app')
+//iniciar a instância do vue com base no componente
+//createApp(App).mount('#app')
+
+const app = createApp(App)
+
+//configurar a instância do pacote mitt como sendo uma propriedade global.
+//essa propriedade estará disponível para todas as instâncias de componente dentro do app.
+app.config.globalProperties.emitter = emitter
+
+//associar a instancia do vue com o elemento html de id app.
+app.mount('#app')
